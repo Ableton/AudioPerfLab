30f7be925094c6ebf49c288c5dcb3e83cfcad567
ryb <ryb@ableton.com>
2019-01-05 16:54:52 +0100

    Add more load to accommodate improved multi-threading performance
    So that dropouts occur when using two threads with either only a busy thread or
    only 80% minimum load for a buffer size of 128.

--- AudioPerfLab/Constants.hpp
+++ AudioPerfLab/Constants.hpp
@@ -8,10 +8,13 @@
 constexpr auto kDefaultNumWorkerThreads = 0;
 constexpr auto kDefaultNumBusyThreads = 0;
 constexpr auto kDefaultPreferredBufferSize = 1024;
-constexpr auto kDefaultNumSines = 30;
+constexpr auto kDefaultNumSines = 60;
 constexpr auto kAmpSmoothingDuration = std::chrono::milliseconds{100};
 constexpr auto kNumPartialsPerProcessingChunk = 32;
-constexpr auto kChordNoteNumbers = {41.0f, 44.0f, 48.0f};
+
+// Play every note twice to increase load
+constexpr auto kChordNoteNumbers = {41.0f, 41.0f, 44.0f, 44.0f, 48.0f, 48.0f};
+
 constexpr auto kRealtimeThreadQuantum = std::chrono::microseconds{500};
 constexpr auto kDriveMeasurementQueueSize = 1024;
 constexpr auto kMaxNumFrames = 4096;
